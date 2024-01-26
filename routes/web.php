858a86324ba6f62b98cfdba76f38efb65ac9fdfb
RhmEdz <vrohim1907@gmail.com>
2024-01-26 19:34:51 +0700

    create login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    \Auth::logout();
+    return redirect('/home');
+});
